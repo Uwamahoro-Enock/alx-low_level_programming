aac4ea312c36439a2a14dfeb99847e65cb4a5ac5
Uwamahoro-Enock <uwamahoroenock608@gmail.com>
2023-03-16 14:37:22 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A code that print a line using printf function
+ * Return: 0 (Go ahead)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n")
+		return (0);
+}
