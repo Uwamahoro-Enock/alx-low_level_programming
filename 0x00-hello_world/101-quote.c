06c89a449edd6e3f16b39363260929d1ce7bf90c
Uwamahoro-Enock <uwamahoroenock608@gmail.com>
2023-03-16 16:07:48 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,9 @@
 int main(void)
 
 {
-	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(STDERR_FILENO, message, 38);
+	char *message1 = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char *message2 = "[stderr]: [Anything\n]";
+	write(STDERR_FILENO, message1, 59);
+	write(STDERR_FILENO, message2, 23);
 	return (1);
 }
