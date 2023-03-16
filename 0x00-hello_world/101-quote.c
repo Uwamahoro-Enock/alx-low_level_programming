47f2d54cfe6fadd274772c3e22d9f4d5842df27e
Uwamahoro-Enock <uwamahoroenock608@gmail.com>
2023-03-16 16:19:22 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
 int main(void)
 
 {
-	char *message1 = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(STDERR_FILENO, message1, 59);
+	write(STDERR_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
 	return (1);
 }
