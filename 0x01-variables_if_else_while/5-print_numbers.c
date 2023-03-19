a43c73f94104f51c4438febd8781da9d86656b02
Uwamahoro-Enock <uwamahoroenock608@gmail.com>
2023-03-19 15:44:56 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -10,10 +10,10 @@
 int main(void)
 
 {
-	int a;
+int a;
 
-	for (a = 0; a < 10; a++)
-	printf("%d", a);
-	printf("\n");
-	return (0);
+for (a = 0; a < 10; a++)
+printf("%d", a);
+printf("\n");
+return (0);
 }
