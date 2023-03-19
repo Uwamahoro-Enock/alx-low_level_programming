7e4207cf0f7d06ae45baa98a6e43627e87292451
Uwamahoro-Enock <uwamahoroenock608@gmail.com>
2023-03-19 14:55:52 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main -Prin the alphabets
+ * Return: you are good to go
+ */
+
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
