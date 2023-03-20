6e037961fee6da60e89128b4c366efbc8af2b211
Uwamahoro-Enock <uwamahoroenock608@gmail.com>
2023-03-20 07:39:02 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,15 +12,15 @@ int main(void)
 {
 	int d;
 
-	for (d = '0'; d <= '9'; d++)
-	{
-	putchar(d);
-	if (d != '9')
-	{
-	putchar(',');
-	putchar(' ');
-        }
-        }
-        putchar('\n');
-	return (0);
+for (d = '0'; d <= '9'; d++)
+{
+putchar(d);
+if (d != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
 }
