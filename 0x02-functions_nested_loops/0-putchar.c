1cb23d8d026682a8e0e2e5668ab18b894054aa9a
Uwamahoro-Enock <uwamahoroenock608@gmail.com>
2023-03-21 12:09:33 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -print the putchar
+ * Return: 0 (successful)
+ */
+
+int main(void)
+{
+	putchar = '_';
+	putchar = 'p';
+	putchar = 'u';
+	putchar = 't';
+	putchar = 'c';
+	putchar = 'h';
+	putchar = 'a';
+	putchar = 'r';
+	return (0);
+}
